5aee4857804e1452cfb97afa2aef54bb4cec9cc3
guswo393 <guswo393@gmail.com>
2026-01-07 12:38:13 +0900

    chore: First commit

---
+++ src/main/java/com/meta/springprepaare/SpringPrepaareApplication.java
@@ -0,0 +1,13 @@
+package com.meta.springprepaare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPrepaareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPrepaareApplication.class, args);
+    }
+
+}
